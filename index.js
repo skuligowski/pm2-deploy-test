d6441da4084345717661befc913d72d9799b5a96
Sebastian Kuligowski <sebastian.kuligowski@comarch.pl>
2019-01-03 00:03:21 +0100

    Config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
+	console.log('get');
   res.send('HEY3!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
